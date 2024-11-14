5d5352da3ee458666c66a2131722922ec30a3e0e
mbroughtonn <mbroughtonn@gmail.com>
2024-11-14 15:15:52 -0600

    Added validations to Game

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,10 @@
 class Game < ApplicationRecord
   belongs_to :publisher
   belongs_to :genre
+
+  validates :title, presence: true
+  validates :release_year, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1900 }
+  validates :rating, presence: true, inclusion: { in: ['E', 'T', 'M', 'E10+', 'RP', 'AO'], message: "%{value} is not a valid rating" }
+  validates :publisher, presence: true
+  validates :genre, presence: true
 end
