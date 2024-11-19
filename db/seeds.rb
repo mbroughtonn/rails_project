1cd3c04b31c37e1414f41f2a4bd68766d3b40792
mbroughtonn <mbroughtonn@gmail.com>
2024-11-19 12:06:33 -0600

    Updated seeds.rb file to include how many publishers, genres, and games there are in the csv file..

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,49 @@
 require "csv"
 
+# Clear existing data
 Game.delete_all
 Genre.delete_all
 Publisher.delete_all
 
+# Load CSV file
 filename = Rails.root.join("db/video_games.csv")
 puts "Loading data from this file: #{filename}"
 
 csv_data = File.read(filename)
-
 games = CSV.parse(csv_data, headers: true, encoding: "utf-8")
 
 games.each do |game|
-    publisher = Publisher.find_or_create_by(name: game["Metadata.Publishers"])
+  # Find or create publisher
+  publisher = Publisher.find_or_create_by(name: game["Metadata.Publishers"])
 
-    if publisher&.valid?
-        # Create the game
-        new_game = publisher.games.create(
-            title: game["Title"],
-            release_year: game["Release.Year"],
-            rating: game["Release.Rating"],
-            publisher: game["Metadata.Publishers"],
-            genre: game["Metadata.Genres"],
-            console: game["Release.Console"]
-        )
-        if !new_game&.valid?
-            puts "Unable to create game: #{game["Title"]}"
-        end
+  if publisher&.valid?
+    # Find or create genre (if there are multiple genres, handle them)
+    genre_names = game["Metadata.Genres"].split(",") # Assumes genres are comma-separated
+    genres = genre_names.map { |name| Genre.find_or_create_by(name: name.strip) }
+
+    # Create the game
+    new_game = Game.new(
+      title: game["Title"],
+      release_year: game["Release.Year"],
+      rating: game["Release.Rating"],
+      console: game["Release.Console"],
+      publisher: publisher # Correct association
+    )
+
+    # Assign the first genre (or handle multiple genres if required)
+    new_game.genre = genres.first
+
+    if new_game.save
+      puts "Created game: #{new_game.title}"
     else
-        puts "This publisher has errors: #{game['Metadata.Publishers']}"
+      puts "Unable to create game: #{game["Title"]}"
+      puts new_game.errors.full_messages
     end
+  else
+    puts "This publisher has errors: #{game['Metadata.Publishers']}"
+  end
 end
 
 puts "There are #{Publisher.count} publishers"
+puts "There are #{Genre.count} genres"
 puts "There are #{Game.count} games"
-
