0affe89d77d32f112c501986c0cc6461ba4c4f59
mbroughtonn <mbroughtonn@gmail.com>
2024-11-19 10:16:05 -0600

    Updated seeds.rb file to include loading the data from the video_games.csv file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require "csv"
+
+Game.delete_all
+Genre.delete_all
+Publisher.delete_all
+
+filename = Rails.root.join("db/video_games.csv")
+
+puts "Loading data from this file: #{filename}"
