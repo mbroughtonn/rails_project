c8e7667c5dc56cf8e505520d6bddd65f06d39035
mbroughtonn <mbroughtonn@gmail.com>
2024-11-19 10:40:59 -0600

    Updated video_games.csv file to delete all of the *blank* publishers.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,18 @@ Genre.delete_all
 Publisher.delete_all
 
 filename = Rails.root.join("db/video_games.csv")
-
 puts "Loading data from this file: #{filename}"
+
+csv_data = File.read(filename)
+
+games = CSV.parse(csv_data, headers: true, encoding: "utf-8")
+
+games.each do |game|
+    publisher = Publisher.create(name: game["Metadata.Publishers"])
+
+    if !publisher&.valid?
+        puts "This publisher has errors: #{game['Metadata.Publishers']}"
+    end
+end
+
+puts "There are #{Publisher.count} publishers."
