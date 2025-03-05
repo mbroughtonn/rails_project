7358385853d00eb94c5f3d6ca7dcb732af1f8c1f
mbroughtonn <mbroughtonn@gmail.com>
2025-03-05 10:40:25 -0600

    Updated teh consoles section to do the same as the games and publishers sections, all 3 are now using pagination successfully.

--- app/controllers/consoles_controller.rb
+++ app/controllers/consoles_controller.rb
@@ -1,9 +1,12 @@
 class ConsolesController < ApplicationController
   def index
-    @consoles = Console.all
+    per_page = 25
+    @consoles = Console.page(params[:page]).per(per_page)
   end
 
   def show
+    per_page = 25
     @console = Console.find(params[:id])
+    @games = @console.games.page(params[:page]).per(per_page)
   end
 end
