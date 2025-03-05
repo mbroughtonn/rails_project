61ad54f84575f66338b3e0f4dbd2309fab4f2f1c
mbroughtonn <mbroughtonn@gmail.com>
2025-03-05 10:36:59 -0600

    Updated the publishers controller to use pagination.
    Updated the publishers view to show the games published by said company in a ordered list using pagination just like the games view does.

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -1,12 +1,17 @@
 class PublishersController < ApplicationController
   def index
+    per_page = 25
     @publishers = Publisher.joins(:games)
                            .select('publishers.*, COUNT(games.id) AS games_count')
                            .group('publishers.id')
                            .order('games_count DESC')
+                           .page(params[:page])
+                           .per(per_page)
   end
 
   def show
+    per_page = 25
     @publisher = Publisher.find(params[:id])
+    @games = @publisher.games.page(params[:page]).per(per_page)
   end
 end
